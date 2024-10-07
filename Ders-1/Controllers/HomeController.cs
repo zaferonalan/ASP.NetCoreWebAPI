4070368b9f6773c8714c1057137e76cbf8a97db4
zafergunayonalan <zafergunay7@gmail.com>
2024-10-07 17:51:22 +0300

    HelloWebAPI

---
+++ Ders-1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using HelloWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebApi.Controllers
+{
+	[ApiController]
+	[Route("home")]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetMessages()
+		{
+			var result = new ResponseModel()
+			{
+				HttpStatus = 200,
+				Message = "Hello APS.Net Core Web API"
+			};
+
+			return Ok(result);
+		}
+	}
+}
--- Ders-1/Program.cs
+++ Ders-1/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Services (Container)
+builder.Services.AddControllers(); // Controllerlarý kullanýcaz
+builder.Services.AddEndpointsApiExplorer(); // Apý keþfedilebilir hale getirdik
+builder.Services.AddSwaggerGen(); // packages' dan indirdik Swagger'ý kullanmak için.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment()) // Uygulamam development oratamýnda olup olmadýðýný kontrol ettim
+{
+	app.UseSwagger();   //Swagger'ý kullanmak için ekledik ama çalýþtýrdýðýmýzda swaggerdan baþlamasý için Properties içindeki launchSettings'e eklememiz gerekiyor.
+	app.UseSwaggerUI();
+}
 
-app.Run();
+
+app.MapControllers();  // Apileri haritalamak,göstermek için eklememiz gerekir aksi taktirde 404 hatasý alýrýz
+
+app.Run(); //Uygulamayý çalýþtýrýcak
